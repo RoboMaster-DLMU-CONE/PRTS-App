23a768de6d06ee87368d22be33e0bdca1a4e57b1
MoonFeather <moonbite233@gmail.com>
2025-05-17 22:59:38 +0800

    add routing and hero component to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import {useState} from "react";
-import {invoke} from "@tauri-apps/api/core";
+import Hero from "./components/Hero.tsx";
 
 function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const name = "PRTS"
-
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        setGreetMsg(await invoke("greet", {name}));
-    }
-
     return (
         <>
-            <button className="btn btn-primary" onClick={greet}>Hello</button>
-            <p>{greetMsg}</p>
+            <Hero/>
         </>
     );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+function Hero() {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+
+                    <button className="btn btn-primary">开始演算</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
