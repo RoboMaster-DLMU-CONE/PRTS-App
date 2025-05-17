eccbd9d5385e483498d38ea9ac710b5fec8a17ac
moonfeather <moonbite233@gmail.com>
2025-05-17 23:21:21 +0800

    remove GSAP dependency and update Hero component layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ function Hero() {
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content text-center">
                 <div className="max-w-md">
-
-                    <button className="btn btn-primary">开始演算</button>
+                    <h1 className="text-5xl font-bold font-sans text-stone-900 mb-5">PRTS</h1>
+                    <button className="btn btn-primary btn-outline">开始演算</button>
                 </div>
             </div>
         </div>
