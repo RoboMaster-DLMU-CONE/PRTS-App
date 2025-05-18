0f9c8bc9097c7932a719ffefb3b28767b0c2df84
MoonFeather <moonbite233@gmail.com>
2025-05-18 19:30:40 +0800

    refactor load and save functions in settings module

--- src/tauri/settings.ts
+++ src/tauri/settings.ts
@@ -5,11 +5,10 @@ export interface Settings {
     show_hero: boolean;
 }
 
-export async function load(): Settings {
-    const cfg = await invoke<Settings>('get_settings');
-    return cfg;
+export async function load(): Promise<Settings> {
+    return await invoke<Settings>('get_settings');
 }
 
 export async function save(cfg: Settings) {
-    await invoke('set_settings', cfg);
+    await invoke('set_settings', {settings: cfg});
 }
