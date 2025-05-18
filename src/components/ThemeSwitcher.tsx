fb6e666662938a66e2c10c81dc3af189bac12759
MoonFeather <moonbite233@gmail.com>
2025-05-18 22:18:23 +0800

    add WindowButtons and ThemeSwitcher components for enhanced UI controls

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,20 @@
+import {ThemeStore} from "../Icon/ThemeStore.tsx";
+import ThemeListElement from "./ThemeListElement.tsx";
+
+function ThemeSwitcher() {
+    return (
+        <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-circle btn-ghost"><ThemeStore/></div>
+            <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
+                <li><ThemeListElement label="Darcula" themeName="darcula"/></li>
+                <li><ThemeListElement label="Nord" themeName="nord"/></li>
+                <li><ThemeListElement label="Acid" themeName="acid"/></li>
+                <li><ThemeListElement label="Lofi" themeName="lofi"/></li>
+                <li><ThemeListElement label="Forest" themeName="forest"/></li>
+                <li><ThemeListElement label="Cyberpunk" themeName="cyberpunk"/></li>
+            </ul>
+        </div>
+    );
+}
+
+export default ThemeSwitcher;
