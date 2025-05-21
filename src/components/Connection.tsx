0ca33e104ee2f8491c2349ec91f0fc64caa88cdd
MoonFeather <moonbite233@gmail.com>
2025-05-21 22:30:53 +0800

    refactor: improve type safety in Connection component and enhance error handling for serial port fetching

--- src/components/Connection.tsx
+++ src/components/Connection.tsx
@@ -1,28 +1,33 @@
+// `src/components/Connection.tsx`
 import {useEffect, useState} from "react";
 import {invoke} from "@tauri-apps/api/core";
 
 function Connection() {
-    const [serialPorts, setSerialPorts] = useState<Array<String>>([])
+    const [serialPorts, setSerialPorts] = useState<string[]>([]);
+
     useEffect(() => {
         async function fetchSerialPorts() {
             try {
-                const result = await invoke<Array<String>>("get_available_serial_port");
-                setSerialPorts(result)
-            } catch (error) {
-                console.log(error);
+                const result = await invoke<string[]>("get_available_serial_port");
+                setSerialPorts(result);
+            } catch {
                 setSerialPorts([]);
             }
         }
 
         fetchSerialPorts();
-
     }, []);
+
     return (
         <div className="card-body">
             <h2 className="card-title">连接</h2>
             <select defaultValue="选择可用串口" className="select">
-                <option disabled={true}>{`${serialPorts.length === 0 ? "未找到可用串口" : "选择可用串口"}`}</option>
-                {serialPorts.map((el) => <option key={el}>{el}</option>)}
+                <option disabled>
+                    {serialPorts.length === 0 ? "未找到可用串口" : "选择可用串口"}
+                </option>
+                {serialPorts.map((el) => (
+                    <option key={el}>{el}</option>
+                ))}
             </select>
         </div>
     );
