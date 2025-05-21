6851c12f43966403483754d40e438e9b75c2ee8c
MoonFeather <moonbite233@gmail.com>
2025-05-21 21:52:22 +0800

    update version to 0.2.0; enhance serial port management with open and close commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,7 +3,13 @@ mod settings;
 
 use settings::{get_settings, set_settings};
 
-use serial::{get_available_serial_port, open_serial_port};
+use serial::{close_serial_port, get_available_serial_port, open_serial_port, SerialPortManager};
+use std::sync::Mutex;
+use tauri::Manager;
+#[derive(Default)]
+struct AppState {
+    port_manager: SerialPortManager,
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -12,8 +18,13 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![get_settings, set_settings])
         .invoke_handler(tauri::generate_handler![
             get_available_serial_port,
-            open_serial_port
+            open_serial_port,
+            close_serial_port,
         ])
+        .setup(|app| {
+            app.manage(Mutex::new(AppState::default()));
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/serial.rs
+++ src-tauri/src/serial.rs
@@ -1,5 +1,42 @@
 use std::time::Duration;
 use thiserror::Error;
+
+use crate::serial::SerialError::MutexError;
+use crate::AppState;
+use serialport::SerialPort;
+use std::collections::HashMap;
+use std::sync::Mutex;
+use tauri::State;
+
+pub struct SerialPortManager {
+    ports: HashMap<String, Box<dyn SerialPort + Send>>,
+}
+
+impl SerialPortManager {
+    pub fn new() -> Self {
+        Self {
+            ports: HashMap::new(),
+        }
+    }
+    pub fn add_port(&mut self, name: String, port: Box<dyn SerialPort + Send>) {
+        self.ports.insert(name, port);
+    }
+
+    pub fn remove_port(&mut self, name: &str) {
+        self.ports.remove(name);
+    }
+
+    pub fn has_port(&self, name: &str) -> bool {
+        self.ports.contains_key(name)
+    }
+}
+
+impl Default for SerialPortManager {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 #[derive(Error, Debug)]
 pub enum SerialError {
     #[error("读取串口列表失败：{0}")]
@@ -11,6 +48,12 @@ pub enum SerialError {
         #[source]
         source: serialport::Error,
     },
+
+    #[error("无法获取串口管理器锁")]
+    MutexError(String),
+
+    #[error("无法找到指定的串口")]
+    FindPortError,
 }
 
 #[tauri::command]
@@ -23,12 +66,39 @@ pub fn get_available_serial_port() -> Result<Vec<String>, String> {
 }
 
 #[tauri::command]
-pub fn open_serial_port(port: String, baud: u32) -> Result<(), String> {
-    match serialport::new(&port, baud)
+pub fn open_serial_port(
+    state: State<'_, Mutex<AppState>>,
+    name: String,
+    baud: u32,
+) -> Result<(), String> {
+    let serial_port = match serialport::new(&name, baud)
         .timeout(Duration::from_millis(100))
         .open()
     {
-        Ok(_) => Ok(()),
-        Err(e) => Err(SerialError::OpenError { port, source: e }.to_string()),
+        Ok(port) => port,
+        Err(e) => {
+            return Err(SerialError::OpenError {
+                port: name,
+                source: e,
+            }
+            .to_string())
+        }
+    };
+    let manager = &mut state.lock().map_err(|e| e.to_string())?.port_manager;
+    manager.add_port(name, serial_port);
+    Ok(())
+}
+
+#[tauri::command]
+pub fn close_serial_port(state: State<'_, Mutex<AppState>>, name: String) -> Result<(), String> {
+    let manager = &mut state
+        .lock()
+        .map_err(|e| MutexError(e.to_string()).to_string())?
+        .port_manager;
+    if manager.has_port(&name) {
+        manager.remove_port(&name);
+        Ok(())
+    } else {
+        Err(SerialError::FindPortError.to_string())
     }
 }
